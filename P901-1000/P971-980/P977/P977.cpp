7698284a92ebb3c4ff8fef7534a30bac0c4047ae
CMinder <minderctrl@gmail.com>
2024-07-17 18:41:18 +0800

    P977 找到0所在位置，其左侧平方后递减，右侧平方后递增，利用归并排序的归并操作，将两边归并在一起

--- P901-1000/P971-980/P977/P977.cpp
+++ P901-1000/P971-980/P977/P977.cpp
@@ -3,11 +3,31 @@ using namespace std;
 class Solution {
   public:
     vector<int> sortedSquares(vector<int> &nums) {
-        //直接平方，调用系统排序
+        int negative = -1;
         for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < 0) { //获取到0所在的位置,0左侧递减，右侧递增
+                negative = i;
+            }
             nums[i] *= nums[i];
         }
-        sort(nums.begin(), nums.end());
+        //采用类似归并排序的归并操作
+        int left = negative == -1 ? -1 : 0; //负数部分平方后最大值
+        int right = nums.size() - 1;        //正数部分平方后最大值
+        if (negative != -1) {
+            vector<int> temp(nums.size());
+            int index = nums.size() - 1; //末尾下标，从后往前填
+            while (left <= right) {
+                if (nums[left] <= nums[right]) {
+                    temp[index--] = nums[right--];
+                } else {
+                    temp[index--] = nums[left++];
+                }
+            }
+            //拷贝到原数组
+            for (int i = 0; i < nums.size(); i++) {
+                nums[i] = temp[i];
+            }
+        }
         return nums;
     }
 };
@@ -19,7 +39,7 @@ void printVector(vector<int> &nums) {
     cout << endl;
 }
 int main() {
-    vector<int> nums = {-7, -3, 2, 3, 11};
+    vector<int> nums = {-5, -3, -2, -1};
     Solution solution;
     nums = solution.sortedSquares(nums);
     printVector(nums);
