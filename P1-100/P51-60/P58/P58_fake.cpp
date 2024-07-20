a64dcd69f33c0489032ccde4985dfeb2837ef8ce
CMinder <minderctrl@gmail.com>
2024-07-20 15:07:24 +0800

    卡码网-P58 求数组某个区间和问题，利用前缀和计算

---
+++ P1-100/P51-60/P58/P58_fake.cpp
@@ -0,0 +1,29 @@
+// 题目描述
+// 给定一个整数数组 Array，请计算该数组在每个指定区间内元素的总和。
+// 输入描述
+// 第一行输入为整数数组 Array 的长度 n，接下来 n
+// 行，每行一个整数，表示数组的元素。随后的输入为需要计算总和的区间下标：a，b
+// （b > = a），直至文件结束。
+//输出描述 输出每个指定区间内元素的总和。
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_LENGTH 100005
+int arr[MAX_LENGTH];
+int calSum(int &start, int &end) {
+    return arr[end] - (start == 0 ? 0 : arr[start - 1]);
+}
+int main() {
+    int n = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        arr[i] += i == 0 ? 0 : arr[i - 1]; //计算累加和
+    }
+    int start = 0;
+    int end = 0;
+    while (cin >> start >> end) {
+        int res = calSum(start, end);
+        cout << res << endl;
+    }
+    return 0;
+}
