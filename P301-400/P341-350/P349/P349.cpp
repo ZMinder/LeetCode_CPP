29b7bbfe40f8ac4611c2936ed57c01c826a6f998
CMinder <minderctrl@gmail.com>
2024-07-30 12:55:58 +0800

    P349 先排序，然后采用双指针扫描，将两个集合相同的元素添加进入结果集中，注意与末尾元素进行比较避免加入相同元素

--- P301-400/P341-350/P349/P349.cpp
+++ P301-400/P341-350/P349/P349.cpp
@@ -3,21 +3,26 @@ using namespace std;
 class Solution {
   public:
     vector<int> intersection(vector<int> &nums1, vector<int> &nums2) {
-        unordered_map<int, bool> map;
-        for (int i = 0; i < nums1.size();
-             i++) { //将nums1中的数加入哈希表并标记为false
-            map[nums1[i]] = false;
-        }
-        for (int i = 0; i < nums2.size(); i++) {
-            if (map.find(nums2[i]) != map.end()) { //将交集标记为true
-                map[nums2[i]] = true;
-            }
-        }
         vector<int> res;
-        for (unordered_map<int, bool>::iterator iter = map.begin();
-             iter != map.end(); iter++) {
-            if (iter->second) { //将标记为true的交集添加进结果集合
-                res.push_back(iter->first);
+        //先对两个集合进行排序
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        //然后采用双指针进行扫描
+        int i = 0;
+        int j = 0;
+        int pre = -1; //表示前一个加入结果集的元素
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] > nums2[j]) {
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                if (nums1[i] != pre) { //与前一个加入结果集合的元素不同
+                    res.push_back(nums1[i]);
+                    pre = nums1[i];
+                }
+                i++;
+                j++;
             }
         }
         return res;
