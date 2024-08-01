f2a2ab23557210b1a2e2a52a15036cf593830029
CMinder <minderctrl@gmail.com>
2024-08-01 17:32:33 +0800

    P454 四数之和（不需要去重），使用哈希表将两重循环将两个数组的累加和及其次数记录下来 再使用两重循环累加另外两个数组的累加和并在哈希表中查找是否存在其相反数 如果存在，累加四元组的数量（哈希表中记录的次数）

---
+++ P401-500/P451-460/P454/P454.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3,
+                     vector<int> &nums4) {
+        int n = nums1.size();
+        int count = 0;
+        unordered_map<int, int> map;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[nums3[i] + nums4[j]]++; //记录后面两个数组的累加和及其次数
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = nums1[i] + nums2[j];
+                if (map.find(-sum) != map.end()) {
+                    count += map[-sum];
+                }
+            }
+        }
+        return count;
+    }
+};
+int mian() { return 0; }
