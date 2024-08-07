35b3b48026b66ef08f29f2ef452bc9cdbc241d1a
CMinder <minderctrl@gmail.com>
2024-08-07 21:16:33 +0800

    KamaCoder-P55 2.在原字符串上进行修改，右移k%str.size()次，每次右移一个字符

--- KamaCoder/P55/P55.cpp
+++ KamaCoder/P55/P55.cpp
@@ -5,11 +5,13 @@ int main() {
     string str;
     cin >> k >> str;
     k %= str.size();
-    for (int i = str.size() - k; i < str.size(); i++) {
-        cout << str[i];
+    for (int i = 0; i < k; i++) { // k次右移 每次右移一个字符
+        char temp = str[str.size() - 1];
+        for (int j = str.size() - 1; j > 0; j--) {
+            str[j] = str[j - 1];
+        }
+        str[0] = temp;
     }
-    for (int i = 0; i < str.size() - k; i++) {
-        cout << str[i];
-    }
+    cout << str;
     return 0;
 }
