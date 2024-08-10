b41395636821d50d5452a4e1b3ba5ff8fe5b91a6
CMinder <minderctrl@gmail.com>
2024-08-10 15:10:56 +0800

    P459 2.str中包含重复子串，假设由n个x长度的子串重复构成，其最长公共前后缀长度必然为（n-1）x 利用这个特点可以利用KMP算法的next数组解决

--- P401-500/P451-460/P459/P459.cpp
+++ P401-500/P451-460/P459/P459.cpp
@@ -3,13 +3,35 @@ using namespace std;
 class Solution {
   public:
     bool repeatedSubstringPattern(string s) {
-        string str = s + s;
-        str.erase(str.end() - 1); //去尾
-        return str.find(s, 1) != -1;//从下标为1的位置开始查找，相当于去头
+        int length = s.size();
+        int next[length];
+        getNext(s, next);
+        if (!next[length - 1]) { //最长公共前后缀长度为0 说明没有重复子串
+            return false;
+        }
+        //假设s由n个x构成，最长公共前后缀长度为m个x，n-m=1
+        return length % (length - next[length - 1]) == 0;
+    }
+
+    void getNext(string &str, int *next) {
+        next[0] = 0;
+        int i = 1;
+        int j = 0;
+        while (i < str.size()) {
+            if (str[i] == str[j]) {
+                next[i++] = ++j;
+            } else {
+                if (j == 0) {
+                    next[i++] = 0;
+                } else {
+                    j = next[j - 1];
+                }
+            }
+        }
     }
 };
 int main() {
-    string str = "aabaaba";
+    string str = "abab";
     Solution solution;
     cout << solution.repeatedSubstringPattern(str) << endl;
     return 0;
