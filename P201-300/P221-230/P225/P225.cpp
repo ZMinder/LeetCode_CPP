487fcfaa18916a42b25b64f89215fa313ec87e36
CMinder <minderctrl@gmail.com>
2024-08-10 21:56:25 +0800

    P225 2.在进栈的时候动手脚，让real队列出队列的顺序与出栈的顺序一致 假设real中已有元素的顺序是满足出栈顺序的，那么只需要在help队列先压入新元素 再将real中元素放入help中，即可满足出栈顺序，之后交换两个队列的指针即可 总结：用队列实现栈，在入栈的时候处理比较合适，用栈实现队列，在出队列的时候处理比较合适 两者都是考虑在入或者出的时候处理

--- P201-300/P221-230/P225/P225.cpp
+++ P201-300/P221-230/P225/P225.cpp
@@ -5,43 +5,28 @@ class MyStack {
     queue<int> *real;
     queue<int> *help;
 
-    void real2help() {
-        while (real->size() > 1) {
-            help->push(real->front());
-            real->pop();
-        }
-    }
-
-    void queueSwap() {
-        queue<int> *temp = real;
-        real = help;
-        help = temp;
-    }
-
   public:
     MyStack() {
         real = new queue<int>();
         help = new queue<int>();
     }
 
-    void push(int x) { real->push(x); }
+    void push(int x) {
+        help->push(x);
+        while (!real->empty()) {
+            help->push(real->front());
+            real->pop();
+        }
+        swap(real, help);
+    }
 
     int pop() {
-        real2help();
-        int last = real->front();
+        int temp = real->front();
         real->pop();
-        queueSwap();
-        return last;
+        return temp;
     }
 
-    int top() {
-        real2help();
-        int last = real->front();
-        help->push(last);
-        real->pop();
-        queueSwap();
-        return last;
-    }
+    int top() { return real->front(); }
 
     bool empty() { return real->empty(); }
 };
