916d2b59091f770ea8b1f26817fd1b4d9f01e978
CMinder <minderctrl@gmail.com>
2024-08-12 18:03:39 +0800

    P1047 利用string本身具有栈的特性来替代栈结构

--- P1001-1100/P1041-1050/P1047/P1047.cpp
+++ P1001-1100/P1041-1050/P1047/P1047.cpp
@@ -3,22 +3,16 @@ using namespace std;
 class Solution {
   public:
     string removeDuplicates(string s) {
-        stack<char> stack;
-        for (int i = s.size() - 1; i >= 0; i--) {
-            //从后往前扫描，保证最后清空栈时字符串是正序
-            if (stack.empty()) {
-                stack.push(s[i]); //如果栈为空，说明i之前字符全部删除
-            } else if (stack.top() == s[i]) {
-                //如果当前字符与栈顶字符相同，说明遇到重复字符
-                stack.pop();
-            } else { //如果不同，说明要添加到最终字符串中
-                stack.push(s[i]);
-            }
-        }
         string res;
-        while (!stack.empty()) {
-            res.append(string(1, stack.top()));
-            stack.pop();
+        //利用string本身存在的栈特性替代栈
+        for (int i = 0; i < s.size(); i++) {
+            if (res.empty()) {
+                res.push_back(s[i]);
+            } else if (res.back() == s[i]) {
+                res.pop_back();
+            } else {
+                (res.push_back(s[i]));
+            }
         }
         return res;
     }
